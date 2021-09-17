3357cc7f45db39ada3f1d27a7c93f6d3c65bd481
aaknox <azhya.knox@gmail.com>
2021-09-17 15:36:47 -0500

    deletion box flickering issue now fixed

--- scripts/deletion-box.js
+++ scripts/deletion-box.js
@@ -1,29 +1,21 @@
 var mouseInArea = false;
 function initializeDeletionBox(){
-    /* Should look like this:
-        <div class="trash-box-container deletion-box" id="trash-box-container">
-            <div class="trash-box">DELETE <i class="fas fa-trash-alt"></i></div>
-        </div>
-    */
-   mouseInArea = true;
-   if(mouseInArea === true){
-       console.log("mouse in deletion area");
-       var trashBoxContainer = document.createElement('div');
-       trashBoxContainer.id = 'trash-box-container';
-       trashBoxContainer.classList.add('trash-box-container', 'deletion-box');
-       var trashBox = document.createElement('div');
-       trashBox.classList.add('trash-box');
-       trashBox.innerHTML = `DELETE <i class="fas fa-trash-alt"></i>`;
-       trashBoxContainer.appendChild(trashBox);
-       document.getElementById('p1').appendChild(trashBoxContainer);
-   }
+    var myObj = document.getElementById('trash-box-container');
+    mouseInArea = true;
+    if(mouseInArea === true){
+        console.log("mouse in deletion area");
+        myObj.style.display = "flex";
+    }
 }
 
 function removeDeletionBox(){
     var myObj = document.getElementById('trash-box-container');
-    myObj.remove();
     setTimeout(()=>{
         mouseInArea = false;
+        myObj.style.display = "none";
         console.log("mouse position in trash area has been reset");
+    }, 200);
+    setTimeout(()=>{
+        myObj.classList.remove("trash-box-container-inside");
     }, 1000);
 }
