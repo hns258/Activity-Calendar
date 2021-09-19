49cf60f02f718dd7783736de1e543840c71c8d28
aaknox <azhya.knox@gmail.com>
2021-09-19 14:28:10 -0500

    fixed deletion box issue, now only triggered when menu is closed

--- scripts/deletion-box.js
+++ scripts/deletion-box.js
@@ -1,10 +1,55 @@
 var mouseInArea = false;
+
 function initializeDeletionBox(){
     var myObj = document.getElementById('trash-box-container');
     mouseInArea = true;
-    if(mouseInArea === true){
+
+    var element = document.querySelector('.sidemenu') || null;
+    if(element === null){
+        //call reverse check here
+        console.log("DEV, YOU NEED TO DO A REVERSE CHECK BECAUSE CLASS DNE!")
+        initializeDeletionBox_L()
+    }else{
+        console.log(element.computedStyleMap().get('right'));
+        var isClosed = false;
+        if(element.computedStyleMap().get('right') < "0px"){
+            console.log("i'm negative!!!")
+            isClosed = true;
+        }else if(element.computedStyleMap().get('right') >= "0px"){
+            console.log("i'm positive boo")
+            isClosed = false;
+        }
+    
+        if(mouseInArea === true && isClosed === true){
+            console.log("mouse in deletion area");
+            myObj.style.display = "flex";
+        }else{
+            console.log("mouse in deletion area BUT menu is also opened!!!")
+        }
+    }
+}
+
+function initializeDeletionBox_L(){
+    var myObj2 = document.getElementById('trash-box-container');
+    mouseInArea2 = true;
+
+    var element2 = document.querySelector('.menuOnLeft');
+
+    console.log(element2.computedStyleMap().get('left'));
+    var isClosed2 = false;
+    if(element2.computedStyleMap().get('left') < "0px"){
+        console.log("i'm negative!!!")
+        isClosed2 = true;
+    }else if(element2.computedStyleMap().get('left') >= "0px"){
+        console.log("i'm positive boo")
+        isClosed2 = false;
+    }
+
+    if(mouseInArea2 === true && isClosed2 === true){
         console.log("mouse in deletion area");
-        myObj.style.display = "flex";
+        myObj2.style.display = "flex";
+    }else{
+        console.log("mouse in deletion area BUT menu is also opened!!!")
     }
 }
 
