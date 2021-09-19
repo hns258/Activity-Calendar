0d9d20d71ca87676d9a4265a9da98204f6c64399
aaknox <azhya.knox@gmail.com>
2021-09-19 18:16:28 -0500

    working on transportation preloading, still buggy atm

--- scripts/alphabetizer.js
+++ scripts/alphabetizer.js
@@ -44,5 +44,55 @@ function abcPeoplePics(){
     xhr.send();
 }
 
+//this function will alphabetize people based on filename
+function abcTransportPics(){
+    console.log(`now alphabetizing the transportation images...`);
+    //1. make a get request for current user setting info
+    //make AJAX request to get current data
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        console.log('ReadyState: ' + xhr.readyState);
+        if (xhr.readyState <= 3) {
+            console.log('loading');
+        }
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(`Success!\n${xhr.responseText}`);
+            //var settings = localStorage.setItem('userSettings', xhr.responseText);
+            var transportJSON = JSON.parse(xhr.responseText);
+            console.log(transportJSON);
+            //2. append images based on new sorted list into table row
+            var transportTableRow = document.querySelector("#transport-imgs-row");
+            transportJSON.forEach((item, index)=>{
+                console.log(`TRANSPORT ${index}: ${item}`);
+                var tableDiv = document.createElement("td");
+                var img = document.createElement("img");
+                img.src = item;
+                tableDiv.appendChild(img);
+                transportTableRow.appendChild(tableDiv);
+            });
+            console.log(transportTableRow);
+            console.log("finished!");
+        }
+        if (xhr.readyState === 4 && xhr.status !== 200) {
+            console.log("Failed. Status Code: " + xhr.status)
+            var reason = {
+                code: xhr.status,
+                issue: 'Failed to load table data from server.'
+                //redirect to error page
+            };
+            console.log(reason);
+            sessionStorage.setItem('failMessage', JSON.stringify(reason));
+            console.log(sessionStorage.getItem('failMessage'));
+        }
+        console.log("Processing")
+    };
+    xhr.open("GET", "https://ac-db-server2.aaknox.repl.co/abc-transport", true);
+    xhr.send();
+}
+
 //call all alphabetizer methods at startup
-abcPeoplePics();
+
+setTimeout(()=>{
+    abcPeoplePics();
+    abcTransportPics();
+}, 1000);
