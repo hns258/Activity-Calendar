064c613f3a7b4506412f74b0ca0b3ed922a0e99b
aaknox <azhya.knox@gmail.com>
2021-10-24 19:28:33 -0500

    disable devtools from showing at startup, testing sonarcloud config

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow() {
   });
   win.loadFile('./public/index.html');
 
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 }
 
 app.whenReady().then(async () => {
