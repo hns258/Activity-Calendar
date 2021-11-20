69b3eb022cbe912cee2f51207fc8d7dc9e7cd7ad
aaknox <azhya.knox@gmail.com>
2021-11-19 18:48:58 -0600

    settings page now showing folder paths from db, changing folders not yet tested

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -12,7 +12,7 @@ const ipcSettingsRenderer = require('electron').ipcRenderer;
 
 // Call to get folder path for specified category
 // returns folder path as a string
-const getFolderLocation = async (category) => {
+async function getFolderLocation(category){
   return ipcSettingsRenderer
     .invoke('get-folder', category)
     .then((folderPath) => {
@@ -22,7 +22,7 @@ const getFolderLocation = async (category) => {
 
 // Call to update folder path in database
 // returns true if folder path update was successful
-const changeFolderLocation = async (category, path) => {
+async function changeFolderLocation(category, path){
   return ipcSettingsRenderer
     .invoke('change-folder', category, path)
     .then((isUpdated) => {
@@ -32,20 +32,18 @@ const changeFolderLocation = async (category, path) => {
 
 /*****************************************************************/
 
-var peopleFileList = localStorage.getItem('peoplePics');
-
-function selectPeopleFolder(e) {
+function selectPeopleFolder(e, typeString) {
   var theFiles = e.target.files;
   var relativePath = theFiles[0].webkitRelativePath;
   var pathShower = document.querySelector('.people-folder-path');
   var folder = relativePath.split('/');
   var folderPath = `C:/documents/activity calender/images/${folder[0]}`;
   pathShower.innerHTML = folderPath;
-  //now saving folderPath to app-settings.json
-  saveToJsonFile(folderPath, 'PEOPLE');
+  //now saving folderPath to db
+  changeFolderLocation(typeString, folderPath);
 }
 
-function selectTransportFolder(e) {
+function selectTransportFolder(e, typeString) {
   var theFiles = e.target.files;
   var relativePath = theFiles[0].webkitRelativePath;
   var pathShower = document.querySelector('.transport-folder-path');
@@ -53,10 +51,10 @@ function selectTransportFolder(e) {
   var folderPath = `C:/documents/activity calender/images/${folder[0]}`;
   pathShower.innerHTML = folderPath;
   //now saving folderPath to app-settings.json
-  saveToJsonFile(folderPath, 'TRANSPORT');
+  changeFolderLocation(typeString, folderPath);
 }
 
-function selectPopularFolder(e) {
+function selectPopularFolder(e, typeString) {
   var theFiles = e.target.files;
   var relativePath = theFiles[0].webkitRelativePath;
   var pathShower = document.querySelector('.popular-folder-path');
@@ -64,10 +62,10 @@ function selectPopularFolder(e) {
   var folderPath = `C:/documents/activity calender/images/${folder[0]}`;
   pathShower.innerHTML = folderPath;
   //now saving folderPath to app-settings.json
-  saveToJsonFile(folderPath, 'POPULAR');
+  changeFolderLocation(typeString, folderPath);
 }
 
-function selectActivityFolder(e) {
+function selectActivityFolder(e, typeString) {
   var theFiles = e.target.files;
   var relativePath = theFiles[0].webkitRelativePath;
   var pathShower = document.querySelector('.activity-folder-path');
@@ -75,92 +73,7 @@ function selectActivityFolder(e) {
   var folderPath = `C:/documents/activity calender/images/${folder[0]}`;
   pathShower.innerHTML = folderPath;
   //now saving folderPath to app-settings.json
-  saveToJsonFile(folderPath, 'ACTIVITY');
-}
-
-function saveToJsonFile(folderStr, locationType) {
-  //make object
-  let temp2 = localStorage.getItem('userSettings');
-  console.log(`Temp test:\n${temp2}`);
-  let temp2Obj = JSON.parse(temp2);
-  let settingsTemp = {
-    image_folder_settings: [
-      {
-        activity_folder_location:
-          temp2Obj.image_folder_settings[0].activity_folder_location,
-        people_folder_location:
-          temp2Obj.image_folder_settings[0].people_folder_location,
-        popular_folder_location:
-          temp2Obj.image_folder_settings[0].popular_folder_location,
-        transport_folder_location:
-          temp2Obj.image_folder_settings[0].transport_folder_location,
-      },
-    ],
-    last_modified: temp2Obj.last_modified,
-  };
-
-  //read json file for previous saved data
-  //fill template with enclosed data
-  setTimeout(() => {
-    //get current datetime
-    var currentdate = new Date();
-    settingsTemp.last_modified =
-      `${currentdate.getMonth()}/${currentdate.getDay()}/${currentdate.getFullYear()} ` +
-      `${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
-
-    //based on location type, modify template to accept new changes
-    switch (locationType) {
-      case 'PEOPLE':
-        console.log('editing people folder location...');
-        settingsTemp.image_folder_settings[0].people_folder_location =
-          folderStr;
-        console.log(`temp being saved:\n ${JSON.stringify(settingsTemp)}`);
-        save(settingsTemp);
-        break;
-      case 'TRANSPORT':
-        console.log('editing transport folder location...');
-        settingsTemp.image_folder_settings[0].transport_folder_location =
-          folderStr;
-        console.log(`temp being saved:\n ${JSON.stringify(settingsTemp)}`);
-        save(settingsTemp);
-        break;
-      case 'POPULAR':
-        console.log('editing popular folder location...');
-        settingsTemp.image_folder_settings[0].popular_folder_location =
-          folderStr;
-        console.log(`temp being saved:\n ${JSON.stringify(settingsTemp)}`);
-        save(settingsTemp);
-        break;
-      case 'ACTIVITY':
-        console.log('editing activity folder location...');
-        settingsTemp.image_folder_settings[0].activity_folder_location =
-          folderStr;
-        console.log(`temp being saved:\n ${JSON.stringify(settingsTemp)}`);
-        save(settingsTemp);
-        break;
-      default:
-        break;
-    }
-  }, 3000);
-}
-
-//send new json data to overwrite json file
-function save(temp) {
-  console.log('now saving settings...');
-  console.log(temp);
-  setTimeout(() => {
-    var newData = JSON.stringify(temp);
-    localStorage.setItem('userSettings', newData);
-    var data = JSON.parse(localStorage.getItem('userSettings'));
-    console.log(data);
-    //now that I have JSON data --> make POST request to server
-    xhttp = new XMLHttpRequest();
-    xhttp.open('POST', 'https://ac-db-server2.aaknox.repl.co/save', true);
-    xhttp.setRequestHeader('Content-type', 'application/json');
-    xhttp.send(JSON.stringify(data));
-    //all done!
-    console.log('done');
-  }, 1000);
+  changeFolderLocation(typeString, folderPath);
 }
 
 //return to index.html page
@@ -174,43 +87,27 @@ function initializeSettings() {
   var page = url[url.length - 1];
   if (page === 'settings.html') {
     console.log("i'm in the settings page");
-    var locationDisplays = document.querySelectorAll('.paths');
-    //make AJAX request to get current data
-    xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      console.log('ReadyState: ' + xhr.readyState);
-      if (xhr.readyState <= 3) {
-        console.log('loading');
-      }
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(`Success!\n${xhr.responseText}`);
-        var settings = localStorage.setItem('userSettings', xhr.responseText);
-        var settingsJSON = JSON.parse(xhr.responseText);
-        //now load the data into the spans
-        locationDisplays[0].innerHTML =
-          settingsJSON.image_folder_settings[0].people_folder_location;
-        locationDisplays[1].innerHTML =
-          settingsJSON.image_folder_settings[0].transport_folder_location;
-        locationDisplays[2].innerHTML =
-          settingsJSON.image_folder_settings[0].popular_folder_location;
-        locationDisplays[3].innerHTML =
-          settingsJSON.image_folder_settings[0].activity_folder_location;
-      }
-      if (xhr.readyState === 4 && xhr.status !== 200) {
-        console.log('Failed. Status Code: ' + xhr.status);
-        var reason = {
-          code: xhr.status,
-          issue: 'Failed to load table data from server.',
-          //redirect to error page
-        };
-        console.log(reason);
-        sessionStorage.setItem('failMessage', JSON.stringify(reason));
-        console.log(sessionStorage.getItem('failMessage'));
-      }
-      console.log('Processing');
-    };
-    xhr.open('GET', 'https://ac-db-server2.aaknox.repl.co/settings', true);
-    xhr.send();
+    var locationDisplayArr = document.querySelectorAll('.paths');
+    //get folder paths from db
+    getFolderLocation('people').then((value) => {
+      //now load the data into the spans
+      locationDisplayArr[0].innerHTML = value;
+    });
+
+    getFolderLocation('transportation').then((value) => {
+      //now load the data into the spans
+      locationDisplayArr[1].innerHTML = value;
+    });
+
+    getFolderLocation('popular').then((value) => {
+      //now load the data into the spans
+      locationDisplayArr[2].innerHTML = value;
+    });
+
+    getFolderLocation('activities').then((value) => {
+      //now load the data into the spans
+      locationDisplayArr[3].innerHTML = value;
+    });
   } else {
     console.log('not the settings page');
   }
