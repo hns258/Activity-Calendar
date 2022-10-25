5b874f9ee3947f350d3b3d9bd05c9f191b997c42
Danny Cho <dannycho7@gmail.com>
2022-10-24 22:32:55 -0400

    support the use of user-data-dir and dependents outside of electron

--- src/user-data-dir.js
+++ src/user-data-dir.js
@@ -3,9 +3,9 @@ const app = require('electron').app;
 
 // Returns the env-dependent base directory to be used by any dynamic user data.
 function getUserDataDir() {
-    return app.isPackaged
+    return app && app.isPackaged
         ? path.join(process.resourcesPath)
-        : path.join(app.getAppPath('userData'));
+        : path.join(__dirname, '..');
 }
 
 module.exports = getUserDataDir;
